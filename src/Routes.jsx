afe1418ae0553b6ede2112686e45e31573ea0977
dna737 <adnanhere27@gmail.com>
2023-08-27 23:26:29 -0500

    add Jewelry and clothing tabs as children of the root parent

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -2,21 +2,22 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import Home from "./components/Home";
 import Electronics from "./components/Electronics";
+import Jewelry from "./components/Jewelry";
+import MClothing from "./components/MClothing";
+import WClothing from "./components/WClothing";
 
 const Router = () => {
     const router = createBrowserRouter([
         {
             path: "/",
             element: <App />,
-            // children: [{ path: "home", element: <HomePage /> }],
             children: [
                 { index: true, element: <Home /> },
                 { path: "electronics", element: <Electronics /> },
+                { path: "jewelry", element: <Jewelry /> },
+                { path: "m-clothing", element: <MClothing /> },
+                { path: "w-clothing", element: <WClothing /> },
             ],
-        },
-        {
-            // path: "profile/:name",
-            // element: <Profile />,
         },
     ]);
 
