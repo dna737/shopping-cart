66f52e00c7c350cc9aa59bf88c7eb5976eb60699
dna737 <adnanhere27@gmail.com>
2023-08-28 18:42:41 -0500

    set up initial cart page

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -4,6 +4,7 @@ import Home from "./components/Home";
 import Electronics from "./components/Electronics";
 import Jewelry from "./components/Jewelry";
 import MClothing from "./components/MClothing";
+import Cart from "./components/Cart";
 import WClothing from "./components/WClothing";
 
 const Router = () => {
@@ -17,6 +18,7 @@ const Router = () => {
                 { path: "jewelry", element: <Jewelry /> },
                 { path: "m-clothing", element: <MClothing /> },
                 { path: "w-clothing", element: <WClothing /> },
+                { path: "cart", element: <Cart /> },
             ],
         },
     ]);
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,16 +10,18 @@ export default function Navbar() {
                         </span>
                     </Link>
                     <div className="flex md:order-2">
-                        <button
-                            type="button"
-                            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                        >
-                            CART
-                        </button>
+                        <Link to="cart">
+                            <button
+                                type="button"
+                                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                            >
+                                CART
+                            </button>
+                        </Link>
                         <button
                             data-collapse-toggle="navbar-sticky"
                             type="button"
-                            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-2k00 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
                             aria-controls="navbar-sticky"
                             aria-expanded="false"
                         >
