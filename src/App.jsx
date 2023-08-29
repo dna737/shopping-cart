023c9a56409032fcab68e97382bba6ea81b921ae
dna737 <adnanhere27@gmail.com>
2023-08-29 11:42:04 -0500

    modify cart from the child component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import { useImmer } from "use-immer";
 import { Outlet, useOutlet } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Cart from "./components/Cart";
 import MClothing from "./components/MClothing";
+// import { useState } from "react";
 
 function App() {
-    const [cart, setCart] = [];
+    const [cart, setCart] = useImmer([]);
+    console.log("🚀 ~ file: App.jsx:11 ~ App ~ cart:", cart);
+    // console.log("🚀 ~ file: App.jsx:9 ~ App ~ setCart:", setCart);
     const outlet = useOutlet();
 
     function handleAddToCart() {
--- src/components/MClothing.jsx
+++ src/components/MClothing.jsx
@@ -3,8 +3,12 @@ import { LoadItems } from "./LoadItems";
 import PropTypes from "prop-types";
 export default function MClothing() {
     const [cart, setCart] = useOutletContext();
+
     function handleAddToCart() {
         console.log("works?");
+        let cartCopy = JSON.parse(JSON.stringify(cart));
+        cartCopy.push("something");
+        setCart(cartCopy);
     }
     return (
         <LoadItems
