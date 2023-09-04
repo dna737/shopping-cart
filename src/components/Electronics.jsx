c746edd53434ef49eee56dc6e652c03b1eaa71bb
dna737 <adnanhere27@gmail.com>
2023-09-03 22:48:13 -0500

    limit product quantities to 10

--- src/components/Electronics.jsx
+++ src/components/Electronics.jsx
@@ -6,8 +6,21 @@ export default function Electronics() {
     const [cart, setCart] = useOutletContext();
 
     function handleAddToCart(product, quantity) {
+        let oldProductIndex = cart.findIndex(
+            (item) => item.product.id === product.id
+        );
         let cartCopy = JSON.parse(JSON.stringify(cart));
-        cartCopy.push({ product: product, quantity: quantity });
+        //this means that the product was already in the cart.
+        if (oldProductIndex !== -1) {
+            cartCopy[oldProductIndex].quantity = Math.min(
+                10,
+                cart[oldProductIndex].quantity + quantity
+            );
+        } else {
+            // this means that the product is a new addition to the cart.
+            // cartCopy =
+            cartCopy.push({ product: product, quantity: quantity });
+        }
         setCart(cartCopy);
     }
     return (
