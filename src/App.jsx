b223e35382cbe7392187c1733a1dedf1b45cbf07
dna737 <adnanhere27@gmail.com>
2023-09-04 18:48:53 -0500

    <Cart /> also uses setCart() now

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,15 @@ import { useImmer } from "use-immer";
 import { Outlet, useOutlet } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
-import Cart from "./components/Cart";
-import MClothing from "./components/MClothing";
-// import { useState } from "react";
 
 function App() {
     const [cart, setCart] = useImmer([]);
-    console.log("🚀 ~ file: App.jsx:11 ~ App ~ cart:", cart);
-    // console.log("🚀 ~ file: App.jsx:9 ~ App ~ setCart:", setCart);
-    const outlet = useOutlet();
 
-    function handleAddToCart() {
-        console.log("hi there");
-        return "hi there";
-    }
-    let component = <MClothing />;
-    console.log("this:", window.location.pathname);
-    // console.log("outlet is instance of cart:", outlet.class() === Cart);
+    console.log("cart:", cart);
     return (
         <>
             <Navbar />
-            {outlet && window.location.pathname.includes("cart") ? (
-                <Outlet cart={cart} handleAddToCart={handleAddToCart} />
-            ) : (
-                <Outlet context={[cart, setCart]} />
-            )}
+            <Outlet context={[cart, setCart]} />
         </>
     );
 }
