240e4c44b283fb2c530b6d19c02939ff0d9f13bf
dna737 <adnanhere27@gmail.com>
2023-09-06 19:37:59 -0500

    initial commit

---
+++ src/tests/App.test.jsx
@@ -0,0 +1,18 @@
+import { render, screen } from "@testing-library/react";
+import App from "../App";
+import { describe, expect, it } from "vitest";
+import { BrowserRouter } from "react-router-dom";
+
+describe("App component", () => {
+    it("renders correct brand name", () => {
+        render(
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        );
+        expect(
+            //use getByText
+            screen.getByText("FAKE_MRKT")
+        ).toBeInTheDocument();
+    });
+});
