03351ab1d98bca33bcabb4d5bda8073bf032f856
dna737 <adnanhere27@gmail.com>
2023-09-09 12:39:51 -0500

    start testing the routing logic

--- src/tests/Cart.test.jsx
+++ src/tests/Cart.test.jsx
@@ -1,4 +1,28 @@
+import { RouterProvider } from "react-router-dom";
+import userEvent from "@testing-library/user-event";
 import { render, screen } from "@testing-library/react";
-import App from "../App";
 import { describe, expect, it } from "vitest";
-import { BrowserRouter } from "react-router-dom";
+import { createMemoryRouter } from "react-router-dom";
+import "react";
+// import { vi } from "vitest";
+
+// import { useState } from "react";
+import { routesConfig } from "../routesConfig.jsx";
+
+describe("App component", () => {
+    it("renders electronic products", async () => {
+        const user = userEvent.setup();
+        const router = createMemoryRouter(routesConfig, {
+            initialEntries: ["/"],
+        });
+        render(<RouterProvider router={router} />);
+        const electronicsTab = screen.getByText(/ELECTRONICS/i);
+        expect(electronicsTab).toBeInTheDocument();
+        await user.click(electronicsTab);
+        expect(
+            screen.getByText(
+                /WD 2TB Elements Portable External Hard Drive - USB 3.0/i
+            )
+        ).toBeInTheDocument();
+    });
+});
