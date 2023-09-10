903b124671beb42d3fed5d324d2aeb14dc88f3fa
dna737 <adnanhere27@gmail.com>
2023-09-10 16:42:31 -0500

    add test case

--- src/tests/Cart.test.jsx
+++ src/tests/Cart.test.jsx
@@ -4,12 +4,10 @@ import { render, screen } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
 import { createMemoryRouter } from "react-router-dom";
 import "react";
-// import { vi } from "vitest";
-
-// import { useState } from "react";
+import { vi } from "vitest";
 import { routesConfig } from "../routesConfig.jsx";
 
-describe("App component", () => {
+describe("Route switching", () => {
     it("routes to the correct page after being clicked on a link", async () => {
         const user = userEvent.setup();
         const router = createMemoryRouter(routesConfig, {
@@ -20,5 +18,26 @@ describe("App component", () => {
         expect(electronicsTab).toBeInTheDocument();
         await user.click(electronicsTab);
         expect(router.state.location.pathname).toEqual("/electronics");
+    });
+});
+
+describe("Cart functionality", () => {
+    it("stores the right item and quantity in the cart", async () => {
+        const user = userEvent.setup();
+        const router = createMemoryRouter(routesConfig, {
+            initialEntries: ["/electronics"],
+        });
+        render(<RouterProvider router={router} />);
+        const cartButton = screen.getByText(/CART/i);
+        expect(cartButton).toBeInTheDocument();
+
+        await user.click(cartButton);
+
+        const mData = [1, 2];
+        const mResponse = {
+            ok: true,
+            json: vi.fn().mockResolvedValue(mData),
+        };
+        global.fetch = vi.fn().mockResolvedValue(mResponse);
     });
 });
